93d75ddbfcc031e56919640720e6b4fb0578cf27
cassioleguizamonbueno <cassioleguizamonbueno@gmail.com>
2022-07-29 17:29:07 -0300

    Integração python com planilha do google drive

---
+++ main.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '14m0oaO1o91UncRQUaZzV5cKsw3c42IxXDa5TDaaPWso'
+SAMPLE_RANGE_NAME = 'Página1!A1:B12'
+
+
+def main():
+
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                     range=SAMPLE_RANGE_NAME).execute()
+
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        # print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[1]))
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
